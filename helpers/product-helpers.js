dbce3be08cefcd186db5a04833c417e9b82c2d71
SayidRadhin <pmradhin@gmail.com>
2021-09-20 21:09:22 +0530

    Added delete ,login setup

---
+++ helpers/product-helpers.js
@@ -0,0 +1,27 @@
+var db=require('../config/connection')
+var collection=require('../config/collections')
+var objectId=require('mongodb').ObjectID
+module.exports={
+
+    addProduct:(product,callback)=>{
+       
+        db.get().collection('product').insertOne(product).then((data)=>{
+            callback(data.insertedId)
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+    },
+    deleteProduct:(prodId)=>{
+        return new Promise((resolve,reject)=>{
+            console.log(prodId);
+            console.log(objectId(prodId));
+            db.get().collection(collection.PRODUCT_COLLECTION).removeOne({_id:objectId(prodId)}).then((response)=>{
+                resolve(response)
+            })
+        })
+    }
+}
